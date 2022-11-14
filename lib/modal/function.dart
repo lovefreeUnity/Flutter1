d1a45b315cffaaf50b8bb68bfd191ba4fc3ee7b3
lovefreeUnity <84775180+lovefreeUnity@users.noreply.github.com>
2022-11-14 10:50:20 +0900

    popup

---
+++ lib/modal/function.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'package:study/modal/custom_popup.dart';
+
+mixin ShowPopup {
+  static void customPopup(
+      {required BuildContext context,
+      required Widget title,
+      required Widget content,
+      required Widget footer}) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) => AlertDialog(
+        title: title,
+        content: content,
+        actions: [footer],
+      ),
+    );
+  }
+
+  static void defaultPopup(
+      {required BuildContext context,
+      required String title,
+      required String content,
+      required String leftButton,
+      required String rightButton,
+      required VoidCallback leftClick,
+      required VoidCallback rightClick,
+      TextStyle? textStyle}) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) => AlertDialog(
+        title: Text(title,
+            textAlign: TextAlign.center,
+            style: textStyle ??
+                const TextStyle(
+                    color: Colors.black, fontWeight: FontWeight.w700)),
+        content: Text(content,
+            textAlign: TextAlign.center,
+            style: textStyle ??
+                const TextStyle(
+                    color: Color(0xFF747474), fontWeight: FontWeight.w400)),
+        actions: [
+          Default(
+              leftButton: leftButton,
+              rightButton: rightButton,
+              leftClick: leftClick,
+              rightClick: rightClick)
+        ],
+      ),
+    );
+  }
+
+  static void horizontal2Popup(
+      {required BuildContext context,
+      required String title,
+      required String content,
+      required String topButton,
+      required String bottomButton,
+      required VoidCallback topClick,
+      required VoidCallback bottomClick,
+      TextStyle? textStyle}) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) => AlertDialog(
+        title: Text(title,
+            textAlign: TextAlign.center,
+            style: textStyle ??
+                const TextStyle(
+                    color: Colors.black, fontWeight: FontWeight.w700)),
+        content: Text(content,
+            textAlign: TextAlign.center,
+            style: textStyle ??
+                const TextStyle(
+                    color: Color(0xFF747474), fontWeight: FontWeight.w400)),
+        actions: [
+          Horizontal_2(
+            topButton: topButton,
+            bottomButton: bottomButton,
+            topClick: topClick,
+            bottomClick: bottomClick,
+          )
+        ],
+      ),
+    );
+  }
+
+  static void horizontal1Popup(
+      {required BuildContext context,
+      required String title,
+      required String content,
+      required String button,
+      required VoidCallback buttonClick,
+      TextStyle? textStyle}) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) => AlertDialog(
+        title: Text(title,
+            textAlign: TextAlign.center,
+            style: textStyle ??
+                const TextStyle(
+                    color: Colors.black, fontWeight: FontWeight.w700)),
+        content: Text(content,
+            textAlign: TextAlign.center,
+            style: textStyle ??
+                const TextStyle(
+                    color: Color(0xFF747474), fontWeight: FontWeight.w400)),
+        actions: [Horizontal_1(button: button, buttonClick: buttonClick)],
+      ),
+    );
+  }
+}
